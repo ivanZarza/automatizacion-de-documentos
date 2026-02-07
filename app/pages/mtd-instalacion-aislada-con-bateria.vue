d7e4768c35886a78b6c68cf595f5ecd7a48ef819
ivanZarza <ivanzarzaestevez@gmail.com>
2026-02-07 08:35:47 +0100

    creando otro archivo y antes de cambiar los parametraos de los inputs

---
+++ app/pages/mtd-instalacion-aislada-con-bateria.vue
@@ -0,0 +1,12 @@
+<script setup>
+import DocumentPage from '../components/DocumentPage.vue'
+import MemoriaTecnica from '../components/MemoriaTecnica.vue'
+import { memoriaInstalacionAisladaConBateriaConfig } from '../config/documents'
+</script>
+
+<template>
+  <DocumentPage 
+    :config="memoriaInstalacionAisladaConBateriaConfig"
+    :documentComponent="MemoriaTecnica"
+  />
+</template>
