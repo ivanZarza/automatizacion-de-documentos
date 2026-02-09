f3e77d04a3dddb10e26acf3be58a965ac02f6455
ivanZarza <ivanzarzaestevez@gmail.com>
2026-02-09 08:46:21 +0100

    Crear contenido válido para página de Memoria Técnica de Puntos de Recarga

---
+++ app/pages/memoria-tecnica-punto-recarga.vue
@@ -0,0 +1,12 @@
+<template>
+  <DocumentPage 
+    :config="memoriaTecnicaPuntoRecargaConfig"
+    :component="MemoriaTecnica"
+  />
+</template>
+
+<script setup>
+import DocumentPage from '~/components/DocumentPage.vue'
+import MemoriaTecnica from '~/components/MemoriaTecnica.vue'
+import { memoriaTecnicaPuntoRecargaConfig } from '~/config/documents'
+</script>
