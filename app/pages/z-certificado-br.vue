2a5454ad98bf8692b88ccfd5a3b6fc1b4de1ac38
ivanZarza <ivanzarzaestevez@gmail.com>
2026-02-15 10:31:31 +0100

    chore(doc): wrap ZCertificadoBR with DocumentPage and pass zCertificadoBRConfig

--- app/pages/z-certificado-br.vue
+++ app/pages/z-certificado-br.vue
@@ -1,7 +1,11 @@
 <script setup>
+import DocumentPage from '../components/DocumentPage.vue'
 import ZCertificadoBR from '../components/ZCertificadoBR.vue'
+import { zCertificadoBRConfig } from '../config/documents'
+
+const config = zCertificadoBRConfig
 </script>
 
 <template>
-  <ZCertificadoBR />
+  <DocumentPage :config="config" :documentComponent="ZCertificadoBR" />
 </template>
