5bebadeb70c9bfb113e0a70f1e0ec408151f3311
ivanZarza <ivanzarzaestevez@gmail.com>
2026-03-03 16:53:49 +0100

    Revertir contraseña de base de datos a la oficial

--- server/utils/db.js
+++ server/utils/db.js
@@ -8,7 +8,7 @@ export function getDbPool() {
       host: process.env.DATABASE_HOST || '51.91.159.188',
       port: parseInt(process.env.DATABASE_PORT) || 5433,
       user: process.env.DATABASE_USER || 'postgres',
-      password: process.env.DATABASE_PASSWORD || 'Postgres123',
+      password: process.env.DATABASE_PASSWORD || 'yLDXHfdOBe3u9o3q19eyDm9EiXSWTuaI2shMDNkp22QjY6qTCoXD4NvtlFRdHGlG',
       database: process.env.DATABASE_NAME || 'postgres'
     })
   }
