4b8e8ffe03fa21bb8b2a4b888e5119dc1988cc77
ivanZarza <ivanzarzaestevez@gmail.com>
2026-03-11 08:48:11 +0100

    buscando fallo numero

--- app/config/documents.js
+++ app/config/documents.js
@@ -2881,7 +2881,7 @@ export const documento80PaginasConfig = {
   },
   fieldMapping: {
     nombre: 'apellidosNombre',
-    direccion: (formData) => `${formData.emplazamientoCalle || ''} - ${formData.numero || ''}    ${formData.bloque || ''} ${formData.escalera || ''}  ${formData.planta || ''}  ${formData.puerta || ''}  `,
+    direccion: (formData) => `${formData.emplazamientoCalle || ''} - ${formData.numero || ''}  ${formData.bloque || ''} ${formData.escalera || ''}  ${formData.planta || ''}  ${formData.puerta || ''}  `,
     numero: 'numero',
     referenciaCatastral: 'referenciaCatastral',
     dia: 'dia',
